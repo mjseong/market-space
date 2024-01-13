b6d083e2b90d8f16c40943c16e520a8b39dd9b16
mjseong <mjseong@flask.global>
2024-01-13 14:39:07 +0900

    project init

---
+++ src/main/java/com/juneox/marketspace/MarketSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.juneox.marketspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juneox/marketspace/MarketSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juneox.marketspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
