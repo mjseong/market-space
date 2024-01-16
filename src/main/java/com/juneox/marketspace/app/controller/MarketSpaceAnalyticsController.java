ab213e199baeb50f7d569970183818041d8870ed
mjseong <mjseong@flask.global>
2024-01-16 16:15:58 +0900

    fix: 스웨거 200번 결과 수정

--- src/main/java/com/juneox/marketspace/app/controller/MarketSpaceAnalyticsController.java
+++ src/main/java/com/juneox/marketspace/app/controller/MarketSpaceAnalyticsController.java
@@ -1,6 +1,7 @@
 package com.juneox.marketspace.app.controller;
 
 import com.juneox.marketspace.app.response.ErrorResponse;
+import com.juneox.marketspace.app.response.ServiceIndustriesWithLowCloseRateResponse;
 import com.juneox.marketspace.app.response.ServiceIndustryNamesResponse;
 import com.juneox.marketspace.common.constant.ErrorConstant;
 import io.swagger.v3.oas.annotations.Operation;
@@ -98,7 +99,43 @@ public interface MarketSpaceAnalyticsController {
     @Operation(summary = "getLowCloseRateStores API")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK",
-                    content = {@Content(schema = @Schema(implementation = ServiceIndustryNamesResponse.class))}),
+                    content = {@Content(schema = @Schema(implementation = ServiceIndustriesWithLowCloseRateResponse.class),
+                    examples = {
+                            @ExampleObject(name = ErrorConstant.INVALID_REQUEST, value = """
+                                        [
+                                            {
+                                            "svc_industry_code_name": "한식음식점",
+                                            "year_and_quarter": "20231",
+                                            "market_space_code": "3110026",
+                                            "biz_close_store_rate": 0
+                                            },
+                                            {
+                                            "svc_industry_code_name": "분식전문점",
+                                            "year_and_quarter": "20231",
+                                            "market_space_code": "3110026",
+                                            "biz_close_store_rate": 0
+                                            },
+                                            {
+                                            "svc_industry_code_name": "커피-음료",
+                                            "year_and_quarter": "20231",
+                                            "market_space_code": "3110026",
+                                            "biz_close_store_rate": 0
+                                            },
+                                            {
+                                              "svc_industry_code_name": "섬유제품",
+                                              "year_and_quarter": "20232",
+                                              "market_space_code": "3110026",
+                                              "biz_close_store_rate": 0
+                                            },
+                                            {
+                                              "svc_industry_code_name": "화초",
+                                              "year_and_quarter": "20232",
+                                              "market_space_code": "3110026",
+                                              "biz_close_store_rate": 0
+                                            }
+                                        ]
+                                    """)
+                    })}),
             @ApiResponse(responseCode = "400", description = "400 client error",
                     content = {@Content(
                             schema = @Schema(implementation = ErrorResponse.class),
